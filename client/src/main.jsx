0efa05669de259a03c9394471b57e4ed6f2a9c66
Garv05 <coolgarv9@gmail.com>
2023-10-15 12:49:58 +0530

    changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import store from "./store/store.jsx";
-import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </Router>
 );
